f42cda113e2903a4a66e32d18bd90d3b9367344a
dmitriqa <dmitri.bovkunovich@gmail.com>
2022-11-01 17:54:48 +0300

    add

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,7 +1,37 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;  //фреймворк - набор инструментов котрый мы будем использовать для чего-то.
 
+import java.util.concurrent.TimeUnit;
+
 public class MainTest {
+
+
+    @Test
+    public static void testSelenium() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://www.browserstack.com/users/sign_in");
+        WebElement username = driver.findElement(By.id("user_email_login"));
+        WebElement password = driver.findElement(By.id("user_password"));
+        WebElement login = driver.findElement(By.name("commit"));
+
+        username.sendKeys("abc@gmail.com");
+        password.sendKeys("your_password");
+        login.click();
+
+        String expectedUrl = "https://live.browserstack.com/dashboard";
+        String actualUrl = driver.getCurrentUrl();
+
+        Assert.assertEquals(actualUrl, expectedUrl);
+
+
+    }
 
     @Test //аннотация. используется как маркер, что это тест.
     void testGetSum() {
